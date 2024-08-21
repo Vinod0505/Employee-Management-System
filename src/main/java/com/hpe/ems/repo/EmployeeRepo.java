0c224ae501a386434e00af49571893302fec1568
vinod0505 <vinodbojaraju@gmail.com>
2024-08-21 13:04:06 +0530

    updateemployee feature completed

--- src/main/java/com/hpe/ems/repo/EmployeeRepo.java
+++ src/main/java/com/hpe/ems/repo/EmployeeRepo.java
@@ -1,5 +1,6 @@
 package com.hpe.ems.repo;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/hpe/ems/utility/ResponseStructure.java
+++ src/main/java/com/hpe/ems/utility/ResponseStructure.java
@@ -1,5 +1,8 @@
 package com.hpe.ems.utility;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ResponseStructure<T> {
 
 	private int statusCode;
@@ -10,20 +13,23 @@ public class ResponseStructure<T> {
 	public int getStatusCode() {
 		return statusCode;
 	}
-	public void setStatusCode(int statusCode) {
+	public ResponseStructure<T> setStatusCode(int statusCode) {
 		this.statusCode = statusCode;
+		return this;
 	}
 	public String getMessage() {
 		return message;
 	}
-	public void setMessage(String message) {
+	public ResponseStructure<T> setMessage(String message) {
 		this.message = message;
+		return this;
 	}
 	public T getData() {
 		return data;
 	}
-	public void setData(T data) {
+	public ResponseStructure<T> setData(T data) {
 		this.data = data;
+		return this;
 	}
 	
 	
