8d5571c42ffbbeec9e7018059d18d59bc94f30d6
vinod0505 <vinodbojaraju@gmail.com>
2024-08-21 12:07:00 +0530

    addEmplyee feature completed

--- src/main/java/com/hpe/ems/controller/EmployeeController.java
+++ src/main/java/com/hpe/ems/controller/EmployeeController.java
@@ -1,18 +1,27 @@
 package com.hpe.ems.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hpe.ems.model.Employee;
 import com.hpe.ems.service.EmployeeService;
 import com.hpe.ems.utility.ResponseStructure;
 
-@RestController
+@RestController("/")
 public class EmployeeController {
 	
 	private EmployeeService employeeService;
 	
-	public ResponseEntity<ResponseStructure<Employee>> addEmployee(Employee employee){
+	
+	public EmployeeController(EmployeeService employeeService) {
+		super();
+		this.employeeService = employeeService;
+	}
+
+	@PostMapping("employees/addEmployee")
+	public ResponseEntity<ResponseStructure<Employee>> addEmployee(@RequestBody Employee employee){
 		return employeeService.addEmployee(employee);
 	}
 
