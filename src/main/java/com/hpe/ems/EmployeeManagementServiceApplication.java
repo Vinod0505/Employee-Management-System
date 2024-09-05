727fbd3a7f6ca537ecdd5fa39a79119afb4b7089
vinod0505 <vinodbojaraju@gmail.com>
2024-09-05 13:22:38 +0530

    made changes in main class

--- src/main/java/com/hpe/ems/EmployeeManagementServiceApplication.java
+++ src/main/java/com/hpe/ems/EmployeeManagementServiceApplication.java
@@ -2,12 +2,17 @@ package com.hpe.ems;
 
 import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.hpe.ems.model.Employee;
 
 @SpringBootApplication
 public class EmployeeManagementServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementServiceApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(EmployeeManagementServiceApplication.class, args);
+		Employee employee = applicationContext.getBean(Employee.class);
 	}
 
 }
